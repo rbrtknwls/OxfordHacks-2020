adab52b3dd8ac15f04b1acd2894aae8e68f0c3ae
Annie <annievampirerox@gmail.com>
2020-11-15 01:39:45 -0500

    added grocery list functionality

---
+++ public/js/grocerylist.js
@@ -0,0 +1,70 @@
+var numDestinations = 2;
+var dests = [];
+var destinations=[];
+
+var inputContainer = document.getElementById("input-container");
+
+for (i = 0; i < 2; i++) {
+  var div = document.createElement("DIV");
+  var input = document.createElement("input");
+
+  div.classList.add("add-input");
+  input.classList.add("map-input");
+  div.id = "inputbox".concat(toString(numDestinations));
+
+  div.appendChild(input);
+  dests.push(div)
+
+  inputContainer.appendChild(dests[i]);
+}
+
+
+document.getElementById('add-dest').addEventListener('click', function () {
+
+  if (numDestinations <= 7) {
+    var div = document.createElement("DIV");
+    var input = document.createElement("input");
+
+    div.classList.add("add-input");
+    input.classList.add("map-input");
+    div.id = "inputbox".concat(toString(numDestinations));
+
+    div.appendChild(input);
+
+    numDestinations += 1
+    dests.push(div)
+
+    for (i = 0; i < dests.length; i++) {
+      inputContainer.appendChild(dests[i]);
+    }
+
+  }
+
+
+});
+
+
+document.getElementById('sub-dest').addEventListener('click', function () {
+
+  if (numDestinations > 2) {
+    numDestinations -= 1;
+    var element = dests.pop()
+
+    document.getElementById(element.id).remove();
+    console.log(dests);
+  }
+
+});
+
+
+
+document.getElementById('send-btn').addEventListener('click', function () {
+
+  destinations=[]
+  for (i = 0; i < dests.length; i++) {
+    destinations.push(dests[i].childNodes[0].value)
+  }
+
+  console.log(destinations);
+
+});
